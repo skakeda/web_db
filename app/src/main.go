c9783d1fda4fa0c8bceb4c895c1a70ee1158d55f
tttttozono <t.o.tozono.1215@gmail.com>
2023-03-02 10:05:51 +0900

    コミットテスト

--- app/src/main.go
+++ app/src/main.go
@@ -12,12 +12,38 @@ import (
 
 func main() {
     http.HandleFunc("/skakeda", skakeda)
+    http.HandleFunc("/tozono", tozono)
     http.ListenAndServe(":8000", nil)
 }
 
 
 func skakeda(w http.ResponseWriter, r *http.Request) {
     db, err := sql.Open("mysql", "root:8jjStzw7@(mysql-container)/test")
+                if err != nil {
+                        panic(err.Error())
+                }
+    defer db.Close()
+
+     rows, err := db.Query("SELECT txt FROM data WHERE id=1")
+                if err != nil {
+                        panic(err.Error())
+                }
+
+    defer rows.Close()
+
+    var body string
+    rows.Next()
+    rows.Scan(&body)
+// log.Print(body)
+    t , err := template.ParseFiles("main.tpl")
+                if err != nil {
+                        panic(err.Error())
+                }
+    t.Execute(w, body)
+}
+
+func tozono(w http.ResponseWriter, r *http.Request) {
+    db, err := sql.Open("mysql", "root:8jjStzw7@(mysql-container)/tozono_db")
                 if err != nil {
                         panic(err.Error())
                 }
--- db/init/init.sql
+++ db/init/init.sql
@@ -12,3 +12,17 @@ CREATE TABLE data
 
 INSERT INTO data (id, txt) VALUES (1, "A thousand Miles");
 INSERT INTO data (id, txt) VALUES (2, "it the hard way");
+
+CREATE SCHEMA tozono_db;
+USE tozono_db;
+
+DROP TABLE IF EXISTS data;
+
+CREATE TABLE data
+(
+  id           INT(10),
+  txt     VARCHAR(40)
+);
+
+INSERT INTO data (id, txt) VALUES (1, "tozono!");
+INSERT INTO data (id, txt) VALUES (2, "tozono!!");
